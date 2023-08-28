5dd13c2ca37bf315b505b605d7ea71a4ce6fe092
syorito-hatsuki <enderdron3@gmail.com>
2023-08-29 00:39:45 +0300

    Init React query

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 export const applicationTitle = 'hatsuHub'
+const client = new QueryClient()
 
 ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 ).render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </QueryClientProvider>
 );
