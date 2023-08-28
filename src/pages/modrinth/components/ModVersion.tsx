6f5f6b8bd373295b86f56a02d64635adf9c428ce
syorito-hatsuki <enderdron3@gmail.com>
2023-08-29 00:50:13 +0300

    Fixed incorrect version showing and react infinity render after switch to React Query

--- src/pages/modrinth/components/ModVersion.tsx
+++ src/pages/modrinth/components/ModVersion.tsx
@@ -1,15 +1,11 @@
-import {useState} from "react";
 import {Typography} from "@mui/material";
 import {useProjectVersion} from "../../../services/modrinth/version";
 
 export function ModVersion(props: {
     slug: string
 }) {
-    const [text, setText] = useState("")
-    const {data, status} = useProjectVersion(props.slug)
-
-    if (status === "error") setText("Version unavailable")
-    if (status === "success") setText(data[0].version_number)
-
-    return (<Typography style={{float: 'right'}} variant={"body2"}>{text}</Typography>)
+    const {data} = useProjectVersion(props.slug)
+    return <Typography style={{float: 'right'}} variant={"body2"}>
+        {data ? data[0].version_number : "Version unavailable"}
+    </Typography>
 }
--- src/services/modrinth/version.ts
+++ src/services/modrinth/version.ts
@@ -3,6 +3,6 @@ import {Version} from "./types";
 import {getProjectVersion} from "../modrinthApi";
 
 export const useProjectVersion = (slug: string) => useQuery<Version[], Error>({
-    queryKey: ['projectVersion'],
-    queryFn: () => getProjectVersion(slug).then(data => data)
+    queryKey: [`projectVersion_${slug}`],
+    queryFn: () => getProjectVersion(slug).then(data => data),
 })
